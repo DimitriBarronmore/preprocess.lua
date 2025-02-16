f519df6c1598c6c1d6589ddf4233f62678ad6851
DimitriBarronmore <d_barronmore@outlook.com>
2025-02-15 17:17:20 -0700

    better directory management, sandbox setup

--- webbuild.lua
+++ webbuild.lua
@@ -16,6 +16,13 @@ usage: %s [input] [output] [options]
 Processes files from the folder [input] and places the result in [output].
 ]]):format(arg[0])
 
+local function is_directory(path)
+	local f = io.popen("file '" .. path .. "'")
+	local txt = f:read("a")
+	f:close()
+	return (string.find(txt, ("%s: directory"):format(path))) and true or false
+end
+
 if #arg < 2 then
 	print(help_txt)
 	return
@@ -27,12 +34,29 @@ for i = 1, #arg do
 	end
 end
 
+if is_directory(arg[2]) then
+	local bool = false
+	while not (bool == "y" or bool == "n") do
+		io.stdout:write("Directory '" .. arg[2] .. "' already exists. Overwrite?  y/n) ")
+		bool = io.stdin:read("l")
+		if bool == "y" then
+			io.popen("rm -r '" .. arg[2] .. "'")
+			io.stdout:write("\nDeleted directory '" .. arg[2] .. "'.\n")
+		elseif bool == "n" then
+			print("aborted")
+			return
+		else
+			print("invalid: try again")
+		end
+	end
+end
+
 local function copy_file(a, b)
 	local i, o, err1, err2
 	i, err1 = io.open(a, "r")
 	o, err2 = io.open(b, "w+")
 	if err2 and err2:find("No such file or directory") then
-		os.execute(("mkdir --p -v %s"):format(b:gsub("/[^/]*$", "")))
+		os.execute(("mkdir --p -v '%s'"):format(b:gsub("/[^/]*$", "")))
 		o, err2 = io.open(b, "w")
 	end
 	assert(i, err1)
@@ -57,30 +81,48 @@ local function file_callback(short, full)
 	local obj = {}
 	obj.shortname = short
 	obj.fullname = full
+	obj.containing_directory = full:gsub("/[^/]*$", "")
+	if obj.containing_directory == "" then
+		obj.containing_directory = "/"
+	end
 	if extensions_to_process[get_extension(short)] then
-		-- print("html file not copied: " .. full)
-		table.insert(files_to_process, obj)
 		obj.metadata = pp.fmfile(arg[1] .. full) or {}
+		if obj.metadata.ignore then
+			copy_file(arg[1] .. full, arg[2] .. full)
+		else
+			table.insert(files_to_process, obj)
+		end
 	else
 		copy_file(arg[1] .. full, arg[2] .. full)
 	end
 	return obj
 end
 
+local function file_sandbox_fix(base_tree, file_obj)
+	local function internal(sbox)
+		sbox.file = file_obj
+		sbox.tree = base_tree[file_obj.containing_directory]
+		sbox.macros["$"] = function(...) return ... end
+	end
+	return internal
+end
+
 local tree = files.returnFiletree(arg[1], nil, file_callback)
 for _, file in ipairs(files_to_process) do
 	check_dir_handle, err = io.open(arg[2] .. file.fullname, "w+")
 	if err and err:find("No such file or directory") then
-		os.execute(("mkdir --p -v %s"):format(arg[2] .. file.fullname:gsub("/[^/]*$", "")))
+		os.execute(("mkdir --p -v %s"):format(arg[2] .. file.containing_directory))
 	end
-	if file.metadata and file.metadata.ignore then
-		copy_file(arg[1] .. file.fullname, arg[2] .. file.fullname)
-	elseif file.metadata and file.metadata.template then
-		local text = pp.getfile(arg[1] .. file.fullname)
-		file.content = text
-		pp.writefile(arg[1] .. file.metadata.template, arg[2] .. file.fullname, {["file"] = file})
+	local sbox_fix = file_sandbox_fix(tree, file)
+	if file.metadata.template then
+		file.content = pp.getfile(arg[1] .. file.fullname)
+		-- we gotta copy the tree rq to get the template
+		local tree2 = tree["/"]
+		tree2:_cd(file.containing_directory)
+		local template_name = tree2[file.metadata.template]
+		pp.writefile(arg[1] .. template_name.fullname, arg[2] .. file.fullname, {__setup_sandbox = sbox_fix})
 	else
-		pp.writefile(arg[1] .. file.fullname, arg[2] .. file.fullname)
+		pp.writefile(arg[1] .. file.fullname, arg[2] .. file.fullname, {__setup_sandbox = sbox_fix})
 	end
 end
 --- testing
